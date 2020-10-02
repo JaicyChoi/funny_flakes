64a0b63b30700885449be877ea83ead2253633ad
jaicychoi <jaicychoi@gamil.com>
2020-10-02 23:37:58 +0900

    swiper.js added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const cart_icon = document.querySelector('.cart_icon');
 const cart_menu = document.querySelector('.cart_menu');
 let menu_click = false;
+let combo_swiper;
 
 cart_icon.addEventListener('click', function(){
     if( menu_click === false ){
@@ -12,3 +13,11 @@ cart_icon.addEventListener('click', function(){
         menu_click = false;
     }
 });
+
+combo_swiper = new Swiper('.group2_wrapper', {
+    slidesPerView: 'auto',
+    spaceBetween: 60,
+    GrabCursor: true
+});
+
+document.querySelectorAll('.swiper-slide')[2].style.marginRight = 0;
