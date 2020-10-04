6fea935bf6ef4b1c24e7159242355e30a78a5add
jaicychoi <jaicychoi@gamil.com>
2020-10-05 06:48:11 +0900

    ie swiper btn added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//allow forEach for IE
+if(window.NodeList && !NodeList.prototype.forEach)
+    NodeList.prototype.forEach = Array.prototype.forEach;
+
 //cart icon
 const cart_icon = document.querySelector('.cart_icon');
 const cart_menu = document.querySelector('.cart_menu');
@@ -12,6 +16,24 @@ cart_icon.addEventListener('click', function(){
         cart_menu.classList.remove('show');
         menu_click = false;
     }
+});
+
+//main menu controll
+const main_menu_li = document.querySelectorAll('.main_menu>li');
+const section = document.querySelectorAll('section');
+
+function get_index(elm){
+    let c = elm.parentNode.children;
+    for( let i = 0 ; i < c.length ; i++ )
+        if( c[i] === elm ) return i;
+}
+
+main_menu_li.forEach(function(li){
+    li.addEventListener('click', function(){
+        if( get_index(li) === 3 )
+            document.querySelector('footer').scrollIntoView({behavior:'smooth'});
+        else section[get_index(li)].scrollIntoView({behavior:'smooth'});
+    });
 });
 
 //mobile menu controll
@@ -31,40 +53,84 @@ moblie_menu_close.addEventListener('click', function(){
     document.body.classList.remove('scroll_lock');
 });
 
-//main menu controll
-const main_menu_li = document.querySelectorAll('.main_menu>li');
-const section = document.querySelectorAll('section');
-
-function get_index(elm){
-    let c = elm.parentNode.children;
-    for( let i = 0 ; i < c.length ; i++ )
-        if( c[i] === elm ) return i;
-}
-// let get_index = elm => [...elm.parentNode.children].indexOf(elm);
-
-main_menu_li.forEach(function(li){
+const mobile_menu_li = document.querySelectorAll('.mobile_menu>li');
+mobile_menu_li.forEach(function(li){
     li.addEventListener('click', function(){
+        mobile_menu_wrapper.classList.remove('show');
+        mobile_menu.classList.remove('show');
+        document.body.classList.remove('scroll_lock');
         if( get_index(li) === 3 )
             document.querySelector('footer').scrollIntoView({behavior:'smooth'});
         else section[get_index(li)].scrollIntoView({behavior:'smooth'});
     });
 });
 
+
 //top button controll
 const top_btn_wrapper = document.querySelector('.top_btn_wrapper');
 const top_btn = document.querySelector('.top_btn_wrapper>button');
 
 window.addEventListener('scroll', function(){
-    if( window.pageYOffset >= document.body.scrollHeight/4 )
+    if( window.pageYOffset >= document.body.scrollHeight/5 )
         top_btn_wrapper.classList.add('show');
-    else if( window.pageYOffset < document.body.scrollHeight/4 )
+    else if( window.pageYOffset < document.body.scrollHeight/5 )
         top_btn_wrapper.classList.remove('show');
 });
 top_btn.addEventListener('click', function(){
     document.querySelector('header').scrollIntoView({behavior:'smooth'});
 });
 
-//combo siwper
+//ie siwper controll
+let agent = navigator.userAgent.toLowerCase();
+let isIE = false;
+const combo_btn_wrapper = document.querySelector('.combo_btn_wrapper');
+const combo_wrapper = document.querySelector('.combo_wrapper');
+const combo_slide = document.querySelectorAll('.swiper-slide');
+const prev_btn = document.querySelectorAll('.combo_btn_wrapper>button')[0];
+const next_btn = document.querySelectorAll('.combo_btn_wrapper>button')[1];
+let drag_icon = document.head.appendChild(document.createElement('style'));
+let move_next = 1;
+
+if( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ){
+    isIE = true;
+    // drag_icon.innerHTML = '.group2:after{content: none;}';
+}
+
+function show_combo_btn(){
+    // if( window.innerWidth <= 980 && isIE === true )
+    if( window.innerWidth <= 980 )
+        combo_btn_wrapper.classList.add('show');
+    else if( window.innerWidth > 980 )
+        combo_btn_wrapper.classList.remove('show');
+}
+
+show_combo_btn();
+
+window.addEventListener('resize', function(){
+    show_combo_btn();
+    if( window.innerWidth >= 1200 )
+        combo_wrapper.style.transform = 'translateX(0)';
+});
+
+prev_btn.addEventListener('click', function(){
+
+});
+next_btn.addEventListener('click', function(){
+    if( window.innerWidth <= 980){
+        combo_wrapper.style.transform = 'translateX('+ -combo_slide[0].offsetWidth * move_next + 'px)';
+    }
+    if( window.innerWidth <= 768 && move_next <= 1 ){
+        console.log(move_next);
+        move_next++;
+        combo_wrapper.style.transform = 'translateX('+ -combo_slide[0].offsetWidth * move_next + 'px)';        
+    }
+    if( window.innerWidth <= 320 && move_next <= 2 ){
+        move_next++;
+        combo_wrapper.style.transform = 'translateX('+ -combo_slide[0].offsetWidth * move_next + 'px)';
+    }
+});//work on btn controll
+
+//combo siwper(except ie)
 let combo_swiper = new Swiper('.group2_wrapper', {
     slidesPerView: 'auto',
     spaceBetween: 60,
@@ -75,4 +141,6 @@ document.querySelectorAll('.swiper-slide')[2].style.marginRight = '0';
 
 window.addEventListener('resize', function(){
     document.querySelectorAll('.swiper-slide')[2].style.marginRight = '0';
+
+    show_combo_btn();
 });
